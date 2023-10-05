83285a072b023f6768ac09fda32f687a4294da90
Liasanna <112963336+Liasanna@users.noreply.github.com>
2023-10-05 19:08:52 +0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+public class MyTest {
+
+    @BeforeEach
+    public void setUp() {
+        // Метод, выполняющийся перед каждым тестовым случаем
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Метод, выполняющийся после каждого тестового случая
+    }
+
+    @Test
+    public void testSomething() {
+        // Тестовый случай
+    }
+
+    @Test
+    public void testAnotherThing() {
+        // Другой тестовый случай
+    }
+}
